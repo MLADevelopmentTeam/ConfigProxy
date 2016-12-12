f42eb12c9841d249a4f6d63c67e15e94da24d556
jhass <agies@Agies.attlocal.net>
2016-12-12 00:59:20 -0500

    protecting against bad data

--- server/components/services/websocket.js
+++ server/components/services/websocket.js
@@ -1,8 +1,12 @@
 'use strict';
+
 var service = {
   socketServer: null,
   broadcast(filter, message) {
     this.socketServer.clients.forEach(function each(client) {
+      if(!client.filter) {
+        return;
+      }
       if(filter.client == client.filter.client && filter.platform == client.filter.platform) {
         client.send(message);
       }
@@ -13,8 +17,12 @@ var service = {
     wss.on('connection', ws => {
       console.log('connection opened');
       ws.on('message', message => {
-        ws.filter = JSON.parse(message);
         console.log('received: %s', message);
+        try {
+          ws.filter = JSON.parse(message);
+        } catch(error) {
+          console.error(error);
+        }
       });
       ws.on('close', () => {
         console.log('connection closed');
