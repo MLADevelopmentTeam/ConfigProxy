ecbfc2ac6f7505ff3228135dee8b687f0ab68ba2
jhass <agies@Agies.local>
2017-02-24 08:42:02 -0500

    updated for size limit

--- server/config/express.js
+++ server/config/express.js
@@ -32,7 +32,7 @@ export default function(app) {
   app.set('view engine', 'html');
   app.use(shrinkRay());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+  app.use(bodyParser.json({ limit: '5mb' }));
   app.use(methodOverride());
   app.use(cookieParser());
 
